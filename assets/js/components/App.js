5735fcede8acf6044d867eba0ec7dfa3c3110dd4
Jonathan Whittle <Jonathan.Whittle@eltoro.com>
2019-04-13 09:26:35 -0400

    stateless functional

--- assets/js/components/App.js
+++ assets/js/components/App.js
@@ -1,63 +1,106 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import Axios from 'axios'
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
+import Axios from 'axios';
 
 class CatalogList extends React.Component {
-	//TODO: Implement fuzzy matching search feature
-	// and sort functionality by title, author, date, etc.
-	// Axios api call for all books based on sort category, 
-	// set state with returned data, and populate list
+  //TODO: Implement fuzzy matching search feature
+  // and sort functionality by title, author, date, etc.
+  // Axios api call for all books based on sort category,
+  // set state with returned data, and populate list
 
-	// Add edit/delete functionality to each list item
+  // Add edit/delete functionality to each list item
 
-	_isMounted = false
-	state = {
-		books: [],
-		fuzzyMatch: []
-	}
+  _isMounted = false;
+  state = {
+    books: [],
+    fuzzyMatch: []
+  };
 
-	componentDidMount(){
-		this._isMounted = true
-		Axios
-			.get('/api/books')
-			.then( res => {
-				if (this._isMounted){
-					this.setState({ books: res.data.books })
-				}	
-			})
-	}
+  componentDidMount() {
+    this._isMounted = true;
+    Axios.get('/api/books').then(res => {
+      if (this._isMounted) {
+        this.setState({ books: res.data.books });
+      }
+    });
+  }
 
-	componentWillUnmount(){
-		this._isMounted = false
-	}
+  componentWillUnmount() {
+    this._isMounted = false;
+  }
 
-	filterData(){
-		//retrieve filtered list of books
-	}
+  filterData() {
+    //retrieve filtered list of books
+  }
 
-	render() {
-		return (
-			<div className="z-depth-3">
-				<div className="collection with-header">
-					<h2 className="collection-header">Books</h2>
-					{
-						this.state.books.map( book => {
-							return (
-								<a href={`/${book.id}`} className="collection-item truncate blue-grey-text" key={book.id}>
-									<strong>{book.title}</strong> by <em>{book.author}</em>, {book.page_count} <small>pgs.</small>, {book.date_published}
-								</a>
-							)
-						} )
-					}
-				</div>
-			</div>
-		)
-	}
+  render() {
+    return (
+      <div className="z-depth-3">
+        <div className="collection with-header">
+          <h2 className="collection-header">Books</h2>
+          {this.state.books.map(book => {
+            return (
+              <a
+                href={`/${book.id}`}
+                className="collection-item truncate blue-grey-text"
+                key={book.id}
+              >
+                <strong>{book.title}</strong> by <em>{book.author}</em>,{' '}
+                {book.page_count} <small>pgs.</small>, {book.date_published}
+              </a>
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
 }
 
+//TODO: Implement fuzzy matching search feature
+// and sort functionality by title, author, date, etc.
+// Axios api call for all books based on sort category,
+// set state with returned data, and populate list
 
+// Add edit/delete functionality to each list item
 
-if (document.getElementById('app')){
-	const element = document.getElementById('app')
-	ReactDOM.render(<CatalogList />, element)
+const ExpCatalogList = () => {
+  const [books, setBooks] = useState([]);
+  const [fuzzyMatch, setFuzzyMatch] = useState([]);
+
+  useEffect(() => {
+    Axios.get('/api/books').then(res => {
+      if (this._isMounted) {
+        this.setState({ books: res.data.books });
+      }
+    });
+  });
+
+  const filterData = () => {
+    //retrieve filtered list of books
+  };
+
+  return (
+    <div className="z-depth-3">
+      <div className="collection with-header">
+        <h2 className="collection-header">Books</h2>
+        {this.state.books.map(book => {
+          return (
+            <a
+              href={`/${book.id}`}
+              className="collection-item truncate blue-grey-text"
+              key={book.id}
+            >
+              <strong>{book.title}</strong> by <em>{book.author}</em>,{' '}
+              {book.page_count} <small>pgs.</small>, {book.date_published}
+            </a>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+if (document.getElementById('app')) {
+  const element = document.getElementById('app');
+  ReactDOM.render(<CatalogList />, element);
 }
