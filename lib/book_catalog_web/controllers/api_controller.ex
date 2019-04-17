8722e017702e903911265046cd8c4522d093ac1e
Jonathan Whittle <Jonathan.Whittle@eltoro.com>
2019-04-16 20:38:46 -0400

    successful pagination with too many pages

--- lib/book_catalog_web/controllers/api_controller.ex
+++ lib/book_catalog_web/controllers/api_controller.ex
@@ -11,11 +11,12 @@ defmodule BookCatalogWeb.APIController do
   @spec index(Plug.Conn.t(), any()) :: Plug.Conn.t()
   def index(conn, _params) do
     books = Repo.all(Book)
-    page_size = conn.query_params["page_size"]
+    page_size = conn.query_params["page_size"] |> String.to_integer
     total_pages = Enum.count(books)
     
+    bookList = apply_pages(books, page_size, total_pages, 1, %{})
   
-    json conn, books
+    json conn, bookList
 
   end
 
@@ -54,24 +55,5 @@ defmodule BookCatalogWeb.APIController do
     apply_pages(Enum.drop(books, page_size), page_size, total_pages - 1, current_page + 1, new_acc)
   end
 
-  @exceptions [NaiveDateTime, DateTime]
-  @bloat [:__meta__, :__struct__, :__cardinality__, :__field__,  :__owner__]
-  
-  defp strip_meta(list) when is_list(list) do
-    Enum.map(list, &strip_meta/1)
-  end
-  
-  defp strip_meta(schema) when is_map(schema) do
-    Map.take(schema, Map.keys(schema) -- @bloat)
-    |> Enum.map(&strip_meta/1) 
-    |> Enum.into(%{})
-  end
-
-  defp strip_meta({key, %{__struct__: struct} = val})
-    when struct in @exceptions, do: {key, val}
-
-  defp strip_meta({key, val}) when is_map(val) or is_list(val) do
-    {key, strip_meta(val)}
-  end
 
 end
